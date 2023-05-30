a135cbd621bd836a5ee3b491c89848adfd3b5cc8
OzhoginCode <danozh.b@gmail.com>
2023-05-30 22:45:29 +0600

    1 step done

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 export default function solution(content){
   // BEGIN
+  const allData = content
+  .split('\r\n')
+  .filter((str) => str.length !== 0)
+  .map((elem) => elem.split(','));
 
+  const weatherData = allData.slice(1);
+
+  const countOfData = weatherData.length;
+  console.log(`Count: ${countOfData}`);
   // END
 }
